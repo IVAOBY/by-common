73c1426caf2d22de827c3728800f78005e579f43
David Kan <davidkan2003@mail.ru>
2023-06-30 21:39:55 +0200

    init #2

--- src/types/training/api/training-type.ts
+++
@@ -1,4 +0,0 @@
-import {ApiType, PartialBy} from "../../__util";
-import {TrainingType} from "../internal/training-type";
-
-export type TrainingTypeDto = PartialBy<ApiType<TrainingType>, 'forumTemplate'> // 'forumTemplate' only shown to T-Staff
