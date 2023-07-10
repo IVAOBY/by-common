bcf2c163352ee0f1400196c1a492d7a21f76b4af
David Kan <davidkan2003@mail.ru>
2023-07-10 17:25:47 +0200

    + training type dtos

--- types/training/api/training-type.ts
+++ types/training/api/training-type.ts
@@ -2,3 +2,6 @@ import {ApiType, PartialBy} from "../../__util";
 import {TrainingType} from "../internal/training-type";
 
 export type TrainingTypeDto = PartialBy<ApiType<TrainingType>, 'forumTemplate'> // 'forumTemplate' only shown to T-Staff
+
+export type TrainingTypeCreationDto = Pick<TrainingTypeDto, 'rating' | 'forumTemplate' | 'description' | 'suffix' | 'isExam' | 'isPractical'>
+export type TrainingTypeUpdateDto = TrainingTypeCreationDto
